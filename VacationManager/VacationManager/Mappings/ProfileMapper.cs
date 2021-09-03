43fd040fc0bc6c80a1f27891570c6ce8404f27a7
Raimy Mejia <raimym@goflowapp.com>
2021-09-03 10:55:06 -0400

    Register services and automapper

---
+++ VacationManager/VacationManager/Mappings/ProfileMapper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using VacationManager.Models.Entities;
+using VacationManager.Models.Requests;
+using VacationManager.Models.Responses;
+
+namespace VacationManager.Mappings
+{
+    public class ProfileMapper : Profile
+    {
+        public ProfileMapper()
+        {
+            CreateMap<Holiday, HolidayResponse>();
+            CreateMap<CreateHolidayRequest, Holiday>();
+            CreateMap<UpdateHolidayRequest, Holiday>();
+
+            CreateMap<Vacation, VacationResponse>()
+                .ForMember(
+                    destination => destination.HolidayName,
+                    member => member.MapFrom(field => field.Holiday.Id)
+                )
+                .ForMember(
+                    destination => destination.IsLongWeekend,
+                    member => member.MapFrom(x => false)
+                )
+                .ForMember(
+                    destination => destination.TotalDays,
+                    member => member.MapFrom(field => (field.To - field.From).TotalDays)
+                )
+                .ForMember(
+                    destination => destination.TotalDays,
+                    member => member.MapFrom(field => (field.To.Date - field.From.Date).TotalDays)
+                );
+        }
+
+        public bool Test(Vacation vacation)
+        {
+            var fromDate = vacation.From.Date;
+
+            var a = new List<DateTime>
+            {
+                fromDate,
+            };
+
+            while (fromDate <= vacation.To.Date)
+            {
+                a.Add(fromDate);
+                fromDate = fromDate.AddDays(1);
+            }
+
+            var daysOfWeek = a.Select(x => x.DayOfWeek);
+
+            
+            if (daysOfWeek.Any(x=> x == DayOfWeek.Sunday || x == DayOfWeek.Saturday)) { }
+        }
+    }
+}
--- VacationManager/VacationManager/Startup.cs
+++ VacationManager/VacationManager/Startup.cs
@@ -1,19 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VacationManager.Mappings;
 using VacationManager.Models.Entities;
+using VacationManager.Repositories;
+using VacationManager.Services;
 
 namespace VacationManager
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
@@ -24,7 +27,13 @@ namespace VacationManager
             services.AddDbContext<VacationManagerContext>(option => option.UseSqlServer("VacationManagerConnection"));
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = "VacationManager", Version = "v1" }));
 
-            // Add scrutor
+            var mappingConfig = new MapperConfiguration(
+                mc => mc.AddProfile(new ProfileMapper())
+            );
+
+            services.AddSingleton(mappingConfig.CreateMapper());
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +50,21 @@ namespace VacationManager
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            var names = new List<string> { "Service", "Repository" };
+
+            services.Scan(
+                scan =>
+                    scan.FromAssemblies(
+                            typeof(HolidayService).Assembly,
+                            typeof(HolidayRepository).Assembly
+                        )
+                        .AddClasses(x => x.Where(c => names.Any(name => c.Name.EndsWith(name))))
+                        .AsMatchingInterface()
+            );
         }
     }
 }
