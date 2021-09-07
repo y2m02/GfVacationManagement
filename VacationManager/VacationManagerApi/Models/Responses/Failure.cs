4e9b55dc62bbb207810a5c8d779302711cb01749
raimyy2m <raimymejia@gmail.com>
2021-09-07 12:15:43 -0400

    Move responses to separate files

---
+++ VacationManager/VacationManagerApi/Models/Responses/Failure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VacationManagerApi.Models.Responses
+{
+    public class Failure : BaseResponse
+    {
+        public Failure(IEnumerable<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public IEnumerable<string> Errors { get; }
+    }
+}
---
+++ VacationManager/VacationManagerApi/Models/Responses/Validation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VacationManagerApi.Models.Responses
+{
+    public class Validation : BaseResponse
+    {
+        public Validation(IEnumerable<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public IEnumerable<string> Errors { get; }
+    }
+}
